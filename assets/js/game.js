0c7fcdb07eecde83fc669a7cce6c460174d32453
nHur2004 <nathan.hur04@gmail.com>
2022-07-18 15:27:02 -0700

    highscore functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -212,12 +212,23 @@ var startGame = function() {
 //function to end the entire game
 var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!");
-    //if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
-    } else {
-        window.alert("You've lost your robot in battle.");
+
+    //check loaclStorage for high score, if none, then 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
     }
+
+    // if player has more moeny than highscore, player ash new higshcore
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + highScore + "!");
+    } else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }
+
     //ask the plaayer if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
